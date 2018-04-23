3c8baa1b8acf013321ab74aa7993fe35ec26b7ec
Remi Kristelijn <rkristelijn@gmail.com>
2018-04-23 08:20:41 +0200

    first commit

---
+++ test/first_spec.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+
+describe('A feature', () => {
+  describe(' A scenario', () => {
+    it('does something expected', () => {
+      assert(true);
+    });
+  });
+});
